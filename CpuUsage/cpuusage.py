d79bcbc76a5fee1d6b1028b7bb377a71ef737bbc
erpostgr25 <er.shashi09@outlook.com>
2025-08-06 23:48:01 +0530

    Cpu Usage Python Programming

--- CpuUsage/cpuusage.py
+++ CpuUsage/cpuusage.py
@@ -1,5 +1,4 @@
 import psutil
-import time
 
 THRESHOLD = 80
 print("Monitoring CPU usage... Press Ctrl+C to stop.\n")
@@ -8,6 +7,5 @@ try:
         usage = psutil.cpu_percent(interval=1)
         if usage > THRESHOLD:
             print(f"Alert! CPU usage exceeds threshold: {usage}%")
-        time.sleep(5)
 except KeyboardInterrupt:
     print("Monitoring stopped.")  
